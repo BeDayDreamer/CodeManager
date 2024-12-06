97abe6f80ea3dadd2797b88b1262155263a6e1fb
Tang.S <shuo.tang@tii-group.com>
2024-12-06 11:25:59 +0100

    updated

--- factoryPattern/productA.c
+++
@@ -1,9 +0,0 @@
-#include "productA.h"
-
-void ProductAIni(ProductA *ProductA, const char *name)
-{
-    strncpy(ProductA->productName, name, sizeof(ProductA->productName) - 1);
-    ProductA->productName[sizeof(ProductA->productName) - 1] = '\0';
-    ProductA->character = 0;
-    ProductA->character2 = NULL;
-}
--- factoryPattern/productB.c
+++
@@ -1,9 +0,0 @@
-#include "productB.h"
-
-void ProductBIni(ProductB *ProductB, const char *name)
-{
-    strncpy(ProductB->productName, name, sizeof(ProductB->productName) - 1);
-    ProductB->productName[sizeof(ProductB->productName) - 1] = '\0';
-    ProductB->character = 0;
-    ProductB->character2 = NULL;
-}
